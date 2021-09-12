3549c7f62299e9eae1840bea42f87906ef26e3b7
Min Ku Nam <mxn1020@M1137.local>
2021-09-12 17:10:57 -0500

    initial commit for GKE

---
+++ src/main/kotlin/com/example/githubactionsgke/GithubActionsGkeApplication.kt
@@ -0,0 +1,11 @@
+package com.example.githubactionsgke
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GithubActionsGkeApplication
+
+fun main(args: Array<String>) {
+	runApplication<GithubActionsGkeApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/githubactionsgke/GithubActionsGkeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.githubactionsgke
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GithubActionsGkeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
